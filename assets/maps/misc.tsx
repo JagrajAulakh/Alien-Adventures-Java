65ff4817e722f966de16a7f570a6a776daff036e
Jigy5000 <Jigy5000@gmail.com>
2020-03-17 17:08:45 -0400

    changed player cos and added new hover sound

--- assets/maps/misc.tsx
+++ assets/maps/misc.tsx
@@ -1,20 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.3" name="misc" tilewidth="16" tileheight="16" tilecount="150" columns="10">
  <image source="../images/sheets/misc_sheet.png" width="160" height="240"/>
- <tile id="4">
-  <animation>
-   <frame tileid="4" duration="300"/>
-   <frame tileid="5" duration="300"/>
-   <frame tileid="6" duration="300"/>
-   <frame tileid="5" duration="300"/>
-  </animation>
- </tile>
- <tile id="7">
-  <animation>
-   <frame tileid="7" duration="300"/>
-   <frame tileid="8" duration="300"/>
-   <frame tileid="9" duration="300"/>
-   <frame tileid="8" duration="300"/>
-  </animation>
- </tile>
 </tileset>
